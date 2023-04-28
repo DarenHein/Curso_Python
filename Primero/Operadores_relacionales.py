632a28c8aa1ddd26f3fef8f2ec038b778f4101fc
Luis Manjarrez carbajal <darenhein@Hotmail.com>
2023-04-28 13:50:12 -0600

    v3

---
+++ Primero/Operadores_relacionales.py
@@ -0,0 +1,33 @@
+#operadores relacionales 
+
+# como su nombre lo indica relaciona dos o mas variables 
+# se ocupan mucho en if else 
+'''
+operadores relacionales son los siguientes 
+
+1 => mayor que = < 
+2 => menor que = > 
+3 => igual que = == (no confundir con el de asignacion)
+4 => direfernte a = != 
+5 => mayor e igual que = <=
+6 => menor e igual que = >= 
+
+lo siguiente es que aremos una pequeña practica donde en terminal  nos dira si es falso o verdadero 
+
+'''
+num = 7
+num2 = 8 
+num3 = 10 
+num4 = 10
+
+resultado = num == num2 
+
+print(resultado) # en el momento de imprimir esto nos dara un calor false por que ?
+# por que 7 no es igual a 8 
+
+resultado2 = num3 != num4
+
+print (resultado2)# al imprimir en pantalla nos saldra que es True verdadero por que num3(10)  ES IGUAL num4(10)
+# al ocupar el operador relacional no es igual estamos mintiendo ya se que es conuso pero con la practica se quita  
+
+
