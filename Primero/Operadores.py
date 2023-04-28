a5889454777851106ddc2e3cefd913fc77f74575
Luis Manjarrez carbajal <darenhein@Hotmail.com>
2023-04-28 13:13:11 -0600

    v2

---
+++ Primero/Operadores.py
@@ -0,0 +1,37 @@
+#Operadores aritmeticos 
+
+'''
+los operadores aritmeticos en python son distintos que en java en muchos aspectos 
+'''
+num = 10 
+num2 = 30
+# suma 
+resultado = num + num2 
+# resta
+resultado2 = num - num2 
+# multiplicaion 
+resultado3 = num * num2 
+# divicion 
+resultado4 = num / num2 
+# NOTA  hay otro operador para la divicion que NO  regresa decimales si no solo enteros 
+# divicion sin decimales 
+resultado5 = num // num2 
+# resto de una divicion 
+resultado = num % num2 
+# potencia 
+resultado = num ** num2 
+
+#por el moemnto son todos ahora un ejercicio basico 
+# ejercicio resuelve la siguiente operacion utilizando los operadores aritmeticos 
+
+# 3**3 (13/5 - (2x4))
+
+numero = 3 
+numero2 = 13 
+numero3 = 5 
+numero4 = 2 
+numero5 = 4 
+
+resultado2 = (numero ** 3 * ((numero2 / numero3) - (numero4 * numero5 ) ))
+
+print("El resultado de la operacion es :" , resultado2)
---
+++ Primero/Tres.py
@@ -0,0 +1,7 @@
+'''
+comentarios de multiples lienas 
+od no me gusto me gusta mas el de java 
+'''
+print("hola tengo comenatrios")
+
+# comentatio una sola linea 
