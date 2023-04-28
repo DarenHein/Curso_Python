5b4200aee303fa532cabdb092e98b88a7331d4d1
Luis Manjarrez carbajal <darenhein@Hotmail.com>
2023-04-28 00:24:00 -0600

    ve

---
+++ Primero/Segundo.py
@@ -0,0 +1,30 @@
+# python es una chuladda de lenguje 
+# en esta seccion veremos el tipado dinamico 
+# tipos de datos en python 
+# concatenar una variable 
+
+# en python hay 3 tipos de datos numerico,cadenas,booleanos
+# no se necesita especificar el tipo de dato 
+
+numero = 10 # => este tipo de dato numero es lo que seria en java el int float y double todo en uno 
+nombre = "Luis" # => este tipo de dato son en java lo que llama String y char igual para cha se usa comilla simple 
+bandera = True
+bandera2 = False # => los bolleanos se espeficican con la primera letra en mayuscula 
+
+# concatenar una variable 
+print("Hola mi nombre es : " , nombre ) # en python solo concatenamos con una simple coma 
+
+#ahora lo mejor VARIABLES DINAMICAS 
+
+#en la linea de codigo num 9 esta la variable numero con un vavlor de 10 a continucaion vea la linea de codigo que se encuentra abajo 
+numero = 20.3 
+#en la linea de codigo numero 20 se encuntra con el mismo nombre pero con un valor diferente esto se puede hacer en java en lo personal no me gusta por que pude llegar a haver confuncion en mi curso casi no lo oucpare pero lo tendre en cuenta 
+
+# en python podemos sumar entero con decimales sin la necidad de tramformarlos 
+
+num = 10
+num2 = 12.5
+resultado = num + num2 
+print ("el resultado de la suma es ",resultado)
+
+#por el momento no tengo quejas de python jejeje 
