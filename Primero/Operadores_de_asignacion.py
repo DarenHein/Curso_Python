56573cf64a92ff42fa12eef01a893fd13af5bd38
Luis Manjarrez carbajal <darenhein@Hotmail.com>
2023-04-28 23:20:32 -0600

    v4

---
+++ Primero/Operadores_de_asignacion.py
@@ -0,0 +1,24 @@
+'''
+los operadores de asignacion 
+
+sirven solo para ahorrar codigo 
+'''
+
+a = 0 
+
+# si le quiero agregar 5 unidades a la varibla a 
+
+a = a + 5 
+
+# al momento de imprimir pantalla dara que a = 5 
+
+# operaores asignacion 
+
+a += 5 # de suma 
+a -= 5 # de resta 
+a *= 5 # de multiplicaion 
+a /= 5 # de divicion 
+a %= 5 # de resto 
+a **= 5 # de potencia 
+
+# son casi lo mismo que los de java s
---
+++ Primero/Salida_de_datps.py
@@ -0,0 +1,15 @@
+'''
+salida de datos por consola 
+
+hay 3 fordas de sacar datos por consola en lo eprsonal me quedo con la ultima 
+
+'''
+nombre = "Luis"
+edad = 30
+
+# primera forma 
+print("mi nombre es ",nombre,"tienes",edad,"años")
+#segunda forma 
+print("hola mi nombre es {} y tengo {} años ".format(nombre,edad))
+# tercera forma y la que pienso qeu voy ya ocupar 
+print(f"hola mi nombre es {nombre} y tengo {edad} años")
