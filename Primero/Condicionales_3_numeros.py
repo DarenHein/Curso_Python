a807de76566be1ad9b7252916c97bb8bc8bf39ce
DarenHein <darenhein@hotmail.com>
2023-04-30 16:23:54 -0600

    ahuevo2

---
+++ Primero/Condicionales_3_numeros.py
@@ -0,0 +1,14 @@
+#realizar un programa que lea 3 numreos y diga caul es el mayor de los 3 
+
+a  = int (input("digita un numero : "))
+b  = int (input("digtia el segundo numero : "))
+c  = int (input ("digita el ultimo numero : "))
+
+if a>b and a>c : 
+    print (f"el nuermo mayor es el {a}")
+elif b>a and b>c : 
+    print (f"el numero mayor es {c}")
+elif c>a and c>b : 
+    print(f"el numero mayor ess {c}")
+else : 
+    print("los 3 numero son iguales")
