d74ca6da5ea15a2e08dc1461fbc99fdd4f688874
luis <darenhein@hotmail.com>
2023-05-19 22:55:44 -0600

    v2

---
+++ For.py
@@ -0,0 +1,12 @@
+'''
+veremos el buvle for en python 
+es diferente a los demas lenguajes de prorgamacion 
+
+'''
+
+palabra = input("ingresa una cadena -> ")
+
+for caracter in palabra: 
+    print(caracter)
+
+print("programa terminado ")
---
+++ For2.py
@@ -0,0 +1,10 @@
+
+
+num = int(input("ingresa el numero de veces que se repite -> "))
+
+cadena = "hola mundo "
+
+for num in cadena < num  : 
+    print (cadena)
+
+print("Programa terminado ...")
